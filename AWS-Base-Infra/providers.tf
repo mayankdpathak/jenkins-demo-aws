c98dc13709cbb0fd6b2879a97dca7f625b853b7a
mayank-pathak-tm <mayank.pathak@toyota.com>
2021-09-16 19:31:33 +0530

    Fifth Commit

--- AWS-Base-Infra/providers.tf
+++ AWS-Base-Infra/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   profile = "~/.aws/config"
   region  = "ap-south-1"
-  shared_credentials_file = "~/.aws/credentials"
+  shared_credentials_file = "C:\Users\ZEUS\.aws\credentials"
 }
