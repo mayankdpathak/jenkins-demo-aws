ec3ff9e9790b88200ea961f1bd695cc088479210
mayank-pathak-tm <mayank.pathak@toyota.com>
2021-09-16 18:27:49 +0530

    Update Commit

---
+++ AWS-Base-Infra/main.tf
@@ -0,0 +1,44 @@
+resource "aws_vpc" "My-VPC-TF" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+}
+
+resource "aws_subnet" "My-VPC-Subnet" {
+  availability_zone = var.availability_zone
+  vpc_id            = aws_vpc.My-VPC-TF.id
+  cidr_block        = "10.0.1.0/24"
+
+
+}
+
+resource "aws_security_group" "vpc-sg-tf" {
+  name        = "vpc-sg-tf"
+  description = "Allow incoming HTTP connections."
+  vpc_id      = aws_vpc.My-VPC-TF.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "myinstance-web1" {
+  availability_zone = var.availability_zone
+  ami               = "ami-00bf4ae5a7909786c"
+  key_name          = var.aws_key_name
+  instance_type     = "t2.micro"
+  subnet_id         = aws_subnet.My-VPC-Subnet.id
+
+}
+
+resource "aws_instance" "myinstance-web2" {
+  availability_zone = var.availability_zone
+  ami               = "ami-00bf4ae5a7909786c"
+  key_name          = var.aws_key_name
+  instance_type     = "t2.micro"
+  subnet_id         = aws_subnet.My-VPC-Subnet.id
+
+}
---
+++ AWS-Base-Infra/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "availability_zone" {
+  default = "ap-south-1a"
+}
+
+variable "profile" {
+  description = "AWS credentials profile you want to use"
+}
+
+variable "aws_key_path" {}
+variable "aws_key_name" {}
+
